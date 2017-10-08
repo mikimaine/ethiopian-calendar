d480f91a3a74e141dd41692ea1761a87d58e33a0
Miki Maine <mikias.amdu@gmail.com>
2017-10-08 13:23:23 +0300

    enable npm module

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mikimiane on 10/8/17.
+ */
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
---
+++ test/test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var expect = require('chai').expect;
+var ethiopianCalander = require('../index');
+
+describe('#ethiopianCalander', function() {
+  //@TODO Test the package.
+});
